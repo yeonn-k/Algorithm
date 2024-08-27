3f0b14c3d18fc2897247564afed022700fdf15d0
yeonn <130032082+yeonn-k@users.noreply.github.com>
2024-08-28 01:40:08 +0900

    Update 체육복.js

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,4 +1,32 @@
-function solution(n, lost, reserve) {
-    var answer = 0;
-    return answer;
-}
+const solution = (n, lost, reserve) => {
+  let answer = n - lost.length;
+  
+  lost.sort();
+  reserve.sort();
+  
+  for(let i = 0; i < lost.length; i ++){
+    if(reserve.indexOf(lost[i]) !== -1){
+      reserve.splice(reserve.indexOf(lost[i]), 1);
+      lost.splice(i, 1);
+      answer ++;
+      // idx 보정
+      i --;
+    }
+  }
+  
+  for ( let i = 0; i < lost.length; i ++){
+    if(reserve.includes(lost[i]-1)){
+      reserve.splice(reserve.indexOf(lost[i]-1), 1);
+      lost.splice(i, 1);
+      answer ++;
+      // idx 보정
+      i --;
+    }
+    if(reserve.includes(lost[i]+1)){
+      reserve.splice(reserve.indexOf(lost[i]+1), 1);
+      lost.splice(i, 1);
+      answer ++;
+      // idx 보정
+      i --;
+    }
+  }
