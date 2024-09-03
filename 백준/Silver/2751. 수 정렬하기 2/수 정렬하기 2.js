bbdb0fcc18575987146bf987f4c44dd90a3ba385
yeonn <130032082+yeonn-k@users.noreply.github.com>
2024-09-03 16:39:14 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1596 ms, Memory: 224184 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,21 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+readline
+  .on("line", (line) => {
+    input.push(line);
+  })
+  .on("close", () => {
+    solution(input);
+  });
+
+const solution = (input) => {
+  input.shift();
+  const result = input.map((el) => parseInt(el));
+
+  console.log(result.sort((a, b) => a - b).join(`\n`));
+};
